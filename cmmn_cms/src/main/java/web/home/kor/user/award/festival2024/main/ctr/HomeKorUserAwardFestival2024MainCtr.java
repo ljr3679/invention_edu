ce3872076b350608a87f09e9db043d30f03e9023
User <User@192.168.200.101>
2024-07-25 17:22:01 +0900

    2024페스티벌 QR 생성으로 인한 반영(css기타 부분 깨짐 수정해야함)

---
+++ cmmn_cms/src/main/java/web/home/kor/user/award/festival2024/main/ctr/HomeKorUserAwardFestival2024MainCtr.java
@@ -0,0 +1,27 @@
+package web.home.kor.user.award.festival2024.main.ctr;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cms.cmmn.CommonVO;
+import cms.mapping.CMSMappingHandler;
+
+@Controller
+@RequestMapping(value="/award/festival2024/")
+public class HomeKorUserAwardFestival2024MainCtr extends CMSMappingHandler {
+	
+	@RequestMapping(value="main.do")
+	public void main(
+			@ModelAttribute("commonVO") CommonVO vo,
+			HttpServletRequest request,
+			ModelMap model
+			) throws Exception {
+		
+		model.addAttribute("layout_option", "main");
+	}	
+}
